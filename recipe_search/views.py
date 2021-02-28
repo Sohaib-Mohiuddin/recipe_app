c66244754efc6e720ccad9bbd637b68e3599b390
Sohaib-Windows <sohaib.mohiuddin@uoit.net>
2021-02-27 20:33:21 -0500

    Added Changes

--- recipe_search/views.py
+++ recipe_search/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views.generic import TemplateView, ListView
 from py_edamam import PyEdamam, Edamam
 
 # Create your views here.
-
-
+user_ip = 'User'
 users = [
 	{
 		'name': 'sohaib',
@@ -18,34 +17,36 @@ users = [
 	}
 ]
 
-# class HomepageView(TemplateView):
-# 	template_name = "home.html"
+def visitor_ip_address(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 def home(request):
-	context = {
-		'users':users
-	}
+	user_ip = visitor_ip_address(request)
+	# print(request.META)
+	context = {}
+	context['users'] = users
+	context['user_ip'] = user_ip
 	return render(request, 'recipe_search/home.html', context)
 
 def search_by_name(request):
-	query=None
-	search_results = []
+	context = {}
 	if request.method == "GET":
 		edamam_conn = PyEdamam(recipes_appid='3281df5a', recipes_appkey='d8593ef2bf3fe138d042e548796c986a')
-		query = request.GET.get('searchRecipe')
-		search_results = edamam_conn.search_recipe(query)
-		print(search_results)
-	return render(request, 'recipe_search/searchbyname.html', {'query' : query,
-											'search_results' : search_results})
+		context['query'] = request.GET.get('searchRecipe')
+		context['search_results'] = edamam_conn.search_recipe(context['query'])
+	return render(request, 'recipe_search/searchbyname.html', context)
 
 def search_by_ingredient(request):
-	query_ingr=None
-	search_results_ingr = []
+	context = {}
 	if request.method == "GET":
 		edamam_conn_ingr = Edamam(food_appid='b4b4db31', food_appkey='1175509923bd6485f3e5f3fc8b88c596')
-		query_ingr = request.GET.get('searchIngredient')
-		search_results_ingr = edamam_conn_ingr.search_food(query_ingr)
-		# print(search_results_ingr)
-	return render(request, 'recipe_search/searchbyingredient.html', {'query_ingr' : query_ingr,
-											'search_results_ingr' : search_results_ingr})
+		context['query_ingr'] = request.GET.get('searchIngredient')
+		context['search_results_ingr'] = edamam_conn_ingr.search_food(context['query_ingr'])
+	return render(request, 'recipe_search/searchbyingredient.html', context)
 
